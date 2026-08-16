37070c3d10d98da713290d4074cd812c2d986c30
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-08-16 14:14:10 +0300

    we're compilling!

--- src/ev.c
+++ src/ev.c
@@ -2,6 +2,8 @@
 
 // IWYU pragma: private
 
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <stdint.h>
 
@@ -13,7 +15,6 @@
 #include "./core/queue.c" // IWYU pragma: export
 #include "./core/time.c" // IWYU pragma: export
 #include "./core/addr.c" // IWYU pragma: export
-#include "./async-fallback.c" // IWYU pragma: export
 #include "./impl/impl.c" // IWYU pragma: export
 
 const char *ev_strerr(ev_code_t code) {
@@ -25,65 +26,65 @@ const char *ev_strerr(ev_code_t code) {
 	}
 }
 
-ev_code_t ev_init(ev_t ev) {
-	ev->active_n = 0;
+// ev_code_t ev_init(ev_t ev) {
+// 	ev->active_n = 0;
 
-	if (evi_impl_init(ev) < 0) goto fail;
-	if (evi_queue_init(ev) < 0) goto fail_sync;
+// 	if (evi_impl_init(ev) < 0) goto fail;
+// 	if (evi_queue_init(ev) < 0) goto fail_sync;
 
-	#ifdef EV_USE_MULTITHREAD
-		evi_pool_init(ev->pool);
-	#endif
+// 	#ifdef EV_USE_MULTITHREAD
+// 		evi_pool_init(ev->pool);
+// 	#endif
 
-	return ev;
-fail_sync:
-	evi_impl_free(ev);
-fail:
-	free(ev);
-	return NULL;
-}
-void ev_free(ev_t ev) {
-	#ifdef EV_USE_MULTITHREAD
-		evi_pool_free(ev->pool);
-	#endif
+// 	return ev;
+// fail_sync:
+// 	evi_impl_free(ev);
+// fail:
+// 	free(ev);
+// 	return NULL;
+// }
+// void ev_free(ev_t ev) {
+// 	#ifdef EV_USE_MULTITHREAD
+// 		evi_pool_free(ev->pool);
+// 	#endif
 
-	evi_queue_free(ev);
-	evi_impl_free(ev);
-	evi_async_free(ev);
-}
+// 	evi_queue_free(ev);
+// 	evi_impl_free(ev);
+// 	evi_async_free(ev);
+// }
 
-bool ev_busy(ev_t ev) {
-	return ev->active_n > 0;
-}
+// bool ev_busy(ev_t ev) {
+// 	return ev->active_n > 0;
+// }
 
-void ev_begin(ev_t ev) {
-	ev->active_n++;
-}
-void ev_end(ev_t ev) {
-	ev->active_n--;
-}
-ev_code_t ev_exec(ev_t ev, void *udata, ev_worker_t worker, void *pargs, bool sync) {
-	(void)sync;
+// void ev_begin(ev_t ev) {
+// 	ev->active_n++;
+// }
+// void ev_end(ev_t ev) {
+// 	ev->active_n--;
+// }
+// ev_code_t ev_exec(ev_t ev, void *udata, ev_worker_t worker, void *pargs, bool sync) {
+// 	(void)sync;
 
-	#ifdef EV_USE_MULTITHREAD
-		if (!sync) {
-			return evi_pool_exec(ev, ev->pool, udata, worker, pargs);
-		}
-	#endif
+// 	#ifdef EV_USE_MULTITHREAD
+// 		if (!sync) {
+// 			return evi_pool_exec(ev, ev->pool, udata, worker, pargs);
+// 		}
+// 	#endif
 
-	ev_begin(ev);
-	int code = worker(pargs);
-	if (code == EV_ECANCELED) return EV_ECANCELED;
+// 	ev_begin(ev);
+// 	int code = worker(pargs);
+// 	if (code == EV_ECANCELED) return EV_ECANCELED;
 
-	return ev_push(ev, udata, code);
-}
+// 	return ev_push(ev, udata, code);
+// }
 
-ev_hnd_t ev_tty_in(ev_t ev) {
-	return ev->in;
-}
-ev_hnd_t ev_tty_out(ev_t ev) {
-	return ev->out;
-}
-ev_hnd_t ev_tty_err(ev_t ev) {
-	return ev->err;
-}
+// ev_hnd_t ev_tty_in(ev_t ev) {
+// 	return ev->in;
+// }
+// ev_hnd_t ev_tty_out(ev_t ev) {
+// 	return ev->out;
+// }
+// ev_hnd_t ev_tty_err(ev_t ev) {
+// 	return ev->err;
+// }
