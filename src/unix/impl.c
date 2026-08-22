c4fc40b439545fcc983e75185481347fbb0017f0
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-08-22 17:18:16 +0300

    fix: not compilling on termux

--- src/unix/impl.c
+++ src/unix/impl.c
@@ -3,6 +3,15 @@
 #pragma once
 
 #define _GNU_SOURCE
+#include <fcntl.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+
 #include <unistd.h>
 #include <netdb.h>
 #include <pwd.h>
@@ -14,14 +23,8 @@
 #include <sys/times.h>
 #include <sys/wait.h>
 
-#include <fcntl.h>
-#include <signal.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include <yaooi/io.h>
 #include <yaooi/conf.h>
--- src/unix/impl.h
+++ src/unix/impl.h
@@ -7,6 +7,7 @@
 #include <dirent.h>
 #include <fcntl.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <termios.h>
 
